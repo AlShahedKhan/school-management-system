b8dd0d796a304efb44debe9e5dd6396119eca9bd
unknown <abdullahalshahedkhan@gmail.com>
2026-07-30 17:58:32 +0600

    udpate

--- resources/views/school/academic/subject/partials/subject-modal.blade.php
+++ resources/views/school/academic/subject/partials/subject-modal.blade.php
@@ -1,3 +1,5 @@
+@props(['showGradeAddButton' => false])
+
 <x-modal.form
     id="subjectModal"
     form-id="subjectForm"
@@ -6,7 +8,7 @@
     title-class="subject-register-modal-title m-0 text-center font-semibold leading-tight text-slate-800"
     fields-class="grid grid-cols-1"
 >
-    @include('school.academic.subject.partials.inc.form')
+    @include('school.academic.subject.partials.inc.form', ['showGradeAddButton' => $showGradeAddButton])
     <x-slot:footer>
         <div class="grid grid-cols-2 gap-3 border-slate-200 bg-white px-6 py-3">
             <x-button.secondary id="cancelSubjectBtn" class="w-full">Close</x-button.secondary>
