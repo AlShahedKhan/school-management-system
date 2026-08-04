a84f7100d1b0932722e017d01299da5df1d100f1
Ahsan-Mahbub <amjubair820@gmail.com>
2026-08-04 20:50:29 +0600

    Fee Discount Service Update

--- app/Services/SchoolFeeDiscountService.php
+++ app/Services/SchoolFeeDiscountService.php
@@ -2,6 +2,8 @@
 
 namespace App\Services;
 
+use App\Models\AdmissionStudent;
+use App\Models\SchoolFeeTemplate;
 use Illuminate\Support\Facades\DB;
 
 class SchoolFeeDiscountService
@@ -90,6 +92,26 @@ class SchoolFeeDiscountService
             : $value;
 
         return max($amount - $discountAmount, 0);
+    }
+
+    public function calculatePayableAmount(SchoolFeeTemplate $template, AdmissionStudent $student): array
+    {
+        $baseAmount = (float) $template->amount;
+        $effectiveAmount = $this->effectiveTotal(
+            (int) $template->school_id,
+            (int) $student->id,
+            $baseAmount,
+            $template->fee_type_name,
+            $template->fee_name
+        );
+
+        $discountAmount = max($baseAmount - $effectiveAmount, 0);
+
+        return [
+            'base_amount' => round($baseAmount, 2),
+            'discount_amount' => round($discountAmount, 2),
+            'payable_amount' => round($effectiveAmount, 2),
+        ];
     }
 
     private function feeTemplate(int $id): ?object
